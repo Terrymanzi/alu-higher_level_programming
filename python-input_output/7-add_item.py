1d5a427e6abd13eb71020ea66b5c108320e624ee
terry <m.terry@alustudent.com>
2024-04-24 06:49:20 -0700

    Task 7 error fixed!

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-'''task 7'''
+"""This script adds all command line arguments to a list and saves it to a JSON file.
+
+If the JSON file does not exist, it will be created. If no arguments are provided,
+the script will still run and save an empty list to the file if it was newly created.
+Existing data in the file will be preserved and new arguments will be added to it.
+"""
 
 import sys
-import os
 import json
 
 def save_to_json_file(my_obj, filename):
-    """Saves an object to a text file using a JSON representation."""
+    """Writes an object to a file using JSON representation."""
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
 
 def load_from_json_file(filename):
-    """Creates an object from a “JSON file”."""
-    with open(filename, 'r') as f:
-        return json.load(f)
+    """Loads an object from a JSON file."""
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 if __name__ == "__main__":
     filename = "add_item.json"
-    
-    # Load existing data or initialize an empty list if the file does not exist.
-    if os.path.exists(filename):
-        items = load_from_json_file(filename)
-    else:
-        items = []
-
-    # Extend the list with command line arguments passed to the script.
+    items = load_from_json_file(filename)
     items.extend(sys.argv[1:])
-    
-    # Save the updated list back to the file.
     save_to_json_file(items, filename)
