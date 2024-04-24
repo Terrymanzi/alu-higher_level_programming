ee29bcbdbe29bbe4e9b144295c1397a25486197b
terry <m.terry@alustudent.com>
2024-04-24 06:56:54 -0700

    Tssk  7 done!

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""This script adds all command line arguments to a list and saves it to a JSON file.
-
-If the JSON file does not exist, it will be created. If no arguments are provided,
-the script will still run and save an empty list to the file if it was newly created.
-Existing data in the file will be preserved and new arguments will be added to it.
-"""
+"""Add all arguments to a Python list and save them to a file."""
 
 import sys
 import json
