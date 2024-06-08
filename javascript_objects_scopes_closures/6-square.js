1e5d94e42ee497bce132007d217bce0f91833be5
terry <m.terry@alustudent.com>
2024-06-08 12:39:39 -0700

    Task 6 dnoe!

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// 6-square.js
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint(c) {
+    const char = c || 'X';
+    if (this.width && this.height) {
+      const row = char.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
