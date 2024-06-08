e7c169232292a1b76a17d0b276ccb4ede1b4ab2c
terry <m.terry@alustudent.com>
2024-06-08 13:29:59 -0700

    Task 6 done!

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
