8d4d10720bc6089a215b881a1d3a176ddde2ea43
terry <m.terry@alustudent.com>
2024-06-08 12:24:12 -0700

    Task 5 done!

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (this.width && this.height) {
+      const row = 'X'.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
+      }
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+
+module.exports = { Rectangle, Square };
