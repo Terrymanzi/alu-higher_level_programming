a6d9a184f490892df20aca3d93eb3f48dbb79c5d
terry <m.terry@alustudent.com>
2024-06-08 11:14:06 -0700

    Task 9 done!

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function add(a, b) {
+  console.log(a + b);
+}
+
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
+
+add(num1, num2);
