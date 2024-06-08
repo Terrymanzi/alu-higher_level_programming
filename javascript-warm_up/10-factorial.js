ca791ddeedc58864e0d076ee74fdb36c162f08d2
terry <m.terry@alustudent.com>
2024-06-08 11:15:46 -0700

    Task 10 dne!

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
+
+console.log(factorial(num));
