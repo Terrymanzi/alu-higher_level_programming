dbb9cae38ba6ac599b84a1f7d380c2ec2aa087fb
terry <m.terry@alustudent.com>
2024-06-08 14:11:24 -0700

    Task 4 error fixed!

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request(apiUrl, function(error, response, body) {
   }
   
   const films = JSON.parse(body).results;
-  const wedgeAntillesFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+  const wedgeAntillesFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') || film.characters.includes('http://swapi.dev/api/people/18/'));
   
   console.log(wedgeAntillesFilms.length);
 });
