cd3a1e69739cef427a82bd63219e415e8239e86f
terry <m.terry@alustudent.com>
2024-06-08 13:19:54 -0700

    Task 3 done!

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  }
+});
