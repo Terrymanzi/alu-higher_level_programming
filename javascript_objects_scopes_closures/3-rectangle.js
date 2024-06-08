7a3d4fd209dbb068d1eaeeb9ae1404faf30cfa4a
terry <m.terry@alustudent.com>
2024-06-08 12:08:27 -0700

    Task 3 done!

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -7,6 +7,15 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print() {
+    if (this.width && this.height) {
+      const row = 'X'.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
+      }
+    }
+  }
 }
 
 module.exports = Rectangle;
