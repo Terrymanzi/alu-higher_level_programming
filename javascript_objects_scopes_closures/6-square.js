4d64a37158a154b2aeb89dad392449b5669f05cc
terry <m.terry@alustudent.com>
2024-06-08 12:45:52 -0700

    ask 7 dne!

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  charPrint(c) {
+  charPrint (c) {
     const char = c || 'X';
     if (this.width && this.height) {
       const row = char.repeat(this.width);
