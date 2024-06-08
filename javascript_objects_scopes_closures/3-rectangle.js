695e2a3cf04e049e47d45fc9793b7594e78874ca
terry <m.terry@alustudent.com>
2024-06-08 13:04:55 -0700

    Task 3,4 errors fixed

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       const row = 'X'.repeat(this.width);
       for (let i = 0; i < this.height; i++) {
