a900f8d8fcf2ec6ed2eb0fa71fbda0629a3580e4
terrymanzi <m.terry@alustudent.com>
2024-06-29 18:13:57 +0000

    Task 1 done!

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,15 @@ number = random.randint(-10000, 10000)
 # Get the last digit of the number
 last_digit = abs(number) % 10
 
-# Determine the output message based on the last digit
+# Determine the sign of the number
+if number > 0:
+    sign = ""
+elif number < 0:
+    sign = "-"
+else:
+    sign = ""
+
+# Determine the output message based on the last digit and sign
 if last_digit > 5:
     message = f"and is greater than 5"
 elif last_digit == 0:
@@ -14,4 +22,4 @@ else:
     message = f"and is less than 6 and not 0"
 
 # Print the output
-print(f"The string Last digit of {number} is {last_digit}, {message}")
+print(f"The string Last digit of {sign}{abs(number)} is {last_digit} {message}")
