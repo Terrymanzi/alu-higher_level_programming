e3c0979b5f76f2f8e228647849d1d9a50b8f4396
terrymanzi <m.terry@alustudent.com>
2024-08-06 14:19:56 +0000

    task 3 done!

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+ke a url and display the values"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """display the contents"""
+    url = sys.argv[1]
+    response = requests.get(url)
+    print("{}".format(response.headers.get('X-Request-Id')))
